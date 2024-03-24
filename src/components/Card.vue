7162bcdc549b3c0475fd12f8c78d4ae62918e41f
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-03-24 10:50:59 +0100

    Added cards

---
+++ src/components/Card.vue
@@ -0,0 +1,155 @@
+<script>
+import products from "./db.json";
+export default {
+  data() {
+    return {
+      products: products.products,
+      newProducts: [],
+    };
+  },
+
+  mounted() {
+    this.newProducts = this.products.map((product) => ({
+      ...product,
+      badges: [
+        ...product.badges,
+        {
+          type: "",
+          value: "",
+        },
+      ],
+    }));
+    console.log(this.newProducts);
+  },
+};
+</script>
+
+<template>
+  <div
+    class="card-wrapper"
+    v-for="(product, index) in newProducts"
+    :key="index"
+  >
+    <div class="card-image">
+      <img :src="`src/assets/img/${product.frontImage}`" alt="" />
+      <img
+        :src="`src/assets/img/${product.backImage}`"
+        alt=""
+        class="hovered-image"
+      />
+    </div>
+    <div class="tag">
+      <!-- applico la classe sostenibilità solo se il .value è uguale a sostenibilità -->
+      <div
+        class="badge"
+        :class="{
+          sost: product.badges[0].value === 'Sostenibilità',
+          discount: product.badges[0].value !== 'Sostenibilità',
+        }"
+      >
+        {{ product.badges[0].value }}
+      </div>
+      <!-- applico la classe discount o una stringa vuota se il value è diverso da sostenibilità o striga vuota -->
+      <div
+        class="badge"
+        :class="{
+          discount: product.badges[1].value !== 'Sostenibilità',
+          '': product.badges[1].value !== '',
+        }"
+      >
+        {{ product.badges[1].value }}
+      </div>
+    </div>
+    <div class="liked" @click="product.isInFavorites = !product.isInFavorites">
+      <div>
+        <span
+          ><i
+            class="fa-solid fa-heart"
+            :class="{ favorite: product.isInFavorites }"
+          ></i
+        ></span>
+      </div>
+    </div>
+    <div class="info">
+      <div class="brand">{{ product.brand }}</div>
+      <div>
+        <h3>{{ product.name }}</h3>
+      </div>
+      <div>
+        <span>{{ product.price }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../../src/assets/style/style.scss";
+.card-wrapper {
+  width: calc(80% / 3);
+  max-width: 460px;
+  margin: 20px 5px 40px;
+  position: relative;
+
+  .card-image {
+    position: relative;
+    width: 100%;
+
+    &:hover .hovered-image {
+      display: block;
+    }
+    .hovered-image {
+      position: absolute;
+      top: 0;
+      display: none;
+    }
+  }
+  .tag {
+    position: absolute;
+    top: 70%;
+    z-index: 3;
+
+    height: 7%;
+    width: 100%;
+    display: flex;
+    .badge {
+      height: 20px;
+      color: rgb(255, 255, 255);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 10px 5px;
+    }
+  }
+  .liked {
+    height: 50px;
+    width: 40px;
+    background-color: rgb(255, 255, 255);
+    position: absolute;
+    top: 20px;
+    right: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .info {
+    height: 20%;
+    width: 100%;
+    margin-left: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    .brand {
+      color: rgb(142, 142, 142);
+    }
+  }
+}
+.discount {
+  background-color: red;
+}
+.sost {
+  background-color: green;
+}
+.favorite {
+  color: red;
+}
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,194 +1,32 @@
 <script>
+import Card from "./Card.vue";
 
-    import {} from '../db.json'; /* QUI??? */ 
-    export default{}
-
+export default {
+  components: {
+    Card,
+  },
+};
 </script>
 
 <template>
-
-<section id="products-page">
-
-<div class="container products-container">
-        
-    <div class="product-page debug">
-        <img src="assets/img/1.webp" class="product-image first-image">
-        <img src="assets/img/1b.webp" class="product-image second-image">
-        <span class="discount"> 50% </span>
-        <span class="sustainability"> Sostenibilità </span>
-        <span class="saved"> &#9829 </span>
+  <div class="wrapper">
+    <div class="container">
+      <Card />
     </div>
-       
-    <div class="product-info">
-        <div class="brand"> Levi's </div>
-        <div class="product-name"> RELAXED FIT TEE UNISEX </div>
-        <div> 
-            <span class="actual-price"> 14,99 &#x20AC </span> 
-            <span class="old-price"> 29,99 &#x20AC </span>
-        </div>
-    </div>
-
-    <div class="product-page debug"> 
-        
-        <img src="assets/img/2.webp" class="product-image first-image">
-        <img src="assets/img/2b.webp" class="product-image second-image">
-        <span class="discount"> 30% </span>
-        <span class="saved"> &#9829 </span>
-
-        <div class="product-info">
-            <div class="brand"> Guess </div>
-            <div class="product-name"> ROSES TEE </div>
-            <div>
-                <span class="actual-price"> 20,99 &#x20AC </span> 
-                <span class="old-price"> 29,99 &#x20AC </span>
-            </div>
-        </div>
-
-    </div>
-
-    <div class="product-page debug"> 
-        
-        <img src="assets/img/3.webp" class="product-image first-image">
-        <img src="assets/img/3b.webp" class="product-image second-image">
-        <span class="discount"> 50% </span>
-        <span class="sustainability"> Sostenibilità </span>
-        <span class="saved"> &#9829 </span>
-
-        <div class="product-info">
-            <div class="brand"> Come zucchero filato </div>
-            <div class="product-name"> VOGLIA DI COLORI PASTELLO </div>
-            <div>
-                <span class="actual-price"> 129,99 &#x20AC </span> 
-                <span class="old-price"> 184,99 &#x20AC </span>
-            </div>
-        </div>
-
-    </div>
-
-</div>    
-
-</section>
-
+  </div>
 </template>
 
 <style lang="scss" scoped>
+.wrapper {
+  padding-top: 60px;
+  display: flex;
 
-.container.products-container{
-    max-width: 1050px;
+  .container {
+    margin: 0 auto;
+    text-align: center;
     display: flex;
+    justify-content: center;
     flex-wrap: wrap;
-    justify-content: space-between;
-    position: relative;
+  }
 }
-
-.product-page{
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    max-width: 335px;
-    max-height: 530px;
-}
-
-.product-image{
-    max-width: 335px;
-    max-height: 485px;
-}
-
-.first-image{
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: 1;
-}
-
-.second-image{
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: 0;
-}
-
-.first-image:hover {
-  display: none;
-}
-
-.discount{
-    position: absolute;
-    display: flex;
-    top: 430px;
-    left: 0;
-    color: white;
-    height: 20px;
-    width: 40px;
-    background-color: #ff0001;
-    z-index: 2;
-    justify-content: center;
-    font-size: x-small;
-    text-align: center;
-    line-height: 20px;
-}
-
-.sustainability{
-    position: absolute;
-    display: flex;
-    top: 430px;
-    left: 45px;
-    color: white;
-    height: 20px;
-    width: 80px;
-    background-color: #038102;
-    z-index: 2;
-    justify-content: center;
-    font-size: x-small;
-    text-align: center;
-    line-height: 20px;
-}
-
-.saved{
-    position: absolute;
-    display: flex;
-    top: 10px;
-    right: 0;
-    height: 35px;
-    width: 35px;
-    background-color: white;
-    z-index: 2;
-    color: black;
-    align-items: center;
-    justify-content: center;
-    font-size: x-large;
-}
-
-.product-info{
-    display: flex;
-    flex-direction: column;
-    max-width: 335px;
-    max-height: 45px;
-    width: 335px;
-    height: 45px;
-    position: absolute;
-    top: 485px;
-}
-
-.brand{
-    font-size: xx-small;
-    color: gray;
-}
-
-.product-name{
-    font-size: x-small;
-    font-weight: bolder;
-}
-
-.actual-price{
-    color: red;
-    font-size: smaller;
-    font-weight: bolder;
-}
-
-.old-price{
-    text-decoration: line-through;
-    font-size: smaller;
-}
-
 </style>
